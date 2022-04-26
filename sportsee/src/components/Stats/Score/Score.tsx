acb9e281d9745924b7e9ea184679de1cfc9ac01e
sebastien-d-fr <contact@sebastien-d.me>
2022-04-26 18:57:51 +0200

    Ajout graphique

---
+++ sportsee/src/components/Stats/Score/Score.tsx
@@ -0,0 +1,40 @@
+import "./Score.css";
+import PropTypes, { InferProps } from "prop-types";
+import { RadialBarChart, RadialBar } from "recharts";
+
+const ScorePropTypes = {
+    score: PropTypes.string.isRequired
+}
+
+type ScorePropTypes = InferProps<typeof ScorePropTypes>;
+Score.propTypes = ScorePropTypes;
+
+const data = [
+  {
+    "fill": "#FF0000",
+    "value": 12
+  },
+  {
+    "fill": "transparent",
+    "value": 100
+  }
+]
+
+function Score({ score }: ScorePropTypes) {
+  return (
+    <div className="score">
+      <span className="type-stats">Score</span>
+        <div className="score-graphique">
+          <RadialBarChart barSize={15} data={data} height={250} innerRadius="100%" startAngle={90} endAngle={450} width={250}>
+            <RadialBar dataKey="value"/>
+          </RadialBarChart>
+          <div className="pourcentage">
+            <span className="nombre-score">12%</span>
+            <span>de votre objectif</span>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Score;
--- sportsee/src/pages/Dashboard/Dashboard.tsx
+++ sportsee/src/pages/Dashboard/Dashboard.tsx
@@ -1,17 +1,25 @@
 import "./Dashboard.css";
 import Prenom from "../../components/Prenom/Prenom";
 import Apport from "../../components/Apport/Apport";
+import Score from "../../components/Stats/Score/Score";
 
 function Dashboard() {
   return (
     <div className="container">
       <Prenom prenom="Thomas" />
       <span className="description">Félicitation ! Vous avez explosé vos objectifs hier 👏</span>
-      <div className="liste-apports">
-        <Apport photo="calories" quantite="1,930kCal" type="Calories" />
-        <Apport photo="proteines" quantite="155g" type="Protéines" />
-        <Apport photo="glucides" quantite="290g" type="Glucides" />
-        <Apport photo="lipides" quantite="50g" type="Lipides" />
+      <div>
+        <div className="liste-stats">
+          <div>Moyenne</div>
+          <div>Toile</div>
+          <Score score="0.12"/>
+        </div>
+        <div className="liste-apports">
+          <Apport photo="calories" quantite="1,930kCal" type="Calories" />
+          <Apport photo="proteines" quantite="155g" type="Protéines" />
+          <Apport photo="glucides" quantite="290g" type="Glucides" />
+          <Apport photo="lipides" quantite="50g" type="Lipides" />
+        </div>
       </div>
     </div>
   );
